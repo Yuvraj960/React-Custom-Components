c333777b50cb8792455481eb2f46ebadc57dbefd
Yuvraj Gupta <yuvrajgupta956@gmail.com>
2024-11-30 10:21:58 +0530

    Added Multi-Select Feature

---
+++ ACCORDION/Multi-Select-Accordion/src/components/data.js
@@ -0,0 +1,29 @@
+const data = [
+    {
+        id: '1',
+        question: "What is React?",
+        answer: "React is a front-end JavaScript library developed by Facebook in 2011."
+    },
+    {
+        id: '2',
+        question: "What is JSX?",
+        answer: "JSX is a syntax extension for JavaScript. It was written to be used with React."
+    },
+    {
+        id: '3',
+        question: "What is a component?",
+        answer: "A component is a modular, portable, replaceable, and reusable set of well-defined functionality that encapsulates its implementation and exporting it as a higher-level interface."
+    },
+    {
+        id: '4',
+        question: "What is state in React?",
+        answer: "State is a built-in React object that contains dynamic data that can change over time."
+    },
+    {
+        id: '5',
+        question: "What is props in React?",
+        answer: "Props is a special keyword in React, which stands for properties and is being used for passing data from one component to another."
+    }
+]
+
+export default data;
