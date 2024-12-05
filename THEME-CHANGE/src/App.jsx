ba9479b6c7eb04659e0b6b3b918531c3e8b71ef2
Yuvraj Gupta <yuvrajgupta956@gmail.com>
2024-12-06 00:19:17 +0530

    Added the Component 8

---
+++ THEME-CHANGE/src/App.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+import Theme from './components/Theme'
+
+const App = () => {
+  return (
+    <div>
+      <Theme />
+    </div>
+  )
+}
+
+export default App
